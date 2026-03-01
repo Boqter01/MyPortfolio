c1d25987c10a70e218e08cc56361262a70a4e001
BOQTER <boqter007@gmail.com>
2026-03-01 16:58:53 +0700

    change

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
       {/* ✨ BACKGROUND EFFECTS */}
       <div className="absolute inset-0 -z-10">
         <div className="absolute top-20 left-1/2 -translate-x-1/2 w-[550px] h-[550px] bg-brand-light/20 blur-[140px] rounded-full" />
-          {/* <Galaxy /> */}
+          <Galaxy />
       </div>
 
       <motion.div  
